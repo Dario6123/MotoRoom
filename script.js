2a172f7f68a8aba8382d96835e50460b2d60bd37
Dario6123 <you@example.com>
2026-04-20 13:41:44 +0300

    retusuri

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ console.log("MotoRoom site încărcat 🚀");
 function toggleTheme() {
     document.body.classList.toggle("light-mode");
 }
+
+let btn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
