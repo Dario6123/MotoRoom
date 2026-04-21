da8303e03620ddb95c5139162d6586e30513f083
Dario6123 <you@example.com>
2026-04-21 10:17:13 +0300

    finalizare aproape

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
 console.log("MotoRoom site încărcat 🚀");
-function toggleTheme() {
-    document.body.classList.toggle("light-mode");
-}
 
-let btn = document.getElementById("backToTop");
+document.addEventListener("DOMContentLoaded", function () {
 
-window.onscroll = function () {
-    if (document.documentElement.scrollTop > 300) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-};
+    // DARK MODE
+    window.toggleTheme = function () {
+        document.body.classList.toggle("light-mode");
+    };
 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+    // BUTON UP
+    let btn = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", function () {
+        if (document.documentElement.scrollTop > 300) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
     });
-}
 
+    window.scrollToTop = function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    // COS
 let total = 0;
+    let count = 0;
 
-function addToCart(nume, pret) {
+   window.addToCart = function (nume, pret) {
     let lista = document.getElementById("cartItems");
 
     let item = document.createElement("li");
-    item.textContent = nume + " - " + pret + "€";
+
+    item.innerHTML = `
+        ${nume} - ${pret}€
+        <button class="remove-btn">❌</button>
+    `;
+
     lista.appendChild(item);
 
     total += pret;
+    count++;
+
     document.getElementById("total").textContent = total;
-}
+    document.getElementById("cartCount").textContent = count;
+
+    // 🔥 BUTON ȘTERGERE
+    item.querySelector(".remove-btn").addEventListener("click", function () {
+        lista.removeChild(item);
+
+        total -= pret;
+        count--;
+
+        document.getElementById("total").textContent = total;
+        document.getElementById("cartCount").textContent = count;
+    });
+};
+    window.toggleCart = function () {
+        let cart = document.getElementById("cartBox");
+
+        if (cart.style.display === "block") {
+            cart.style.display = "none";
+        } else {
+            cart.style.display = "block";
+        }
+    };
+
+});
